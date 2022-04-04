cc0c0e6dea81028efcfb3ba96991630dbad50650
hailong <1132555965@QQ.COM>
2022-04-04 13:55:13 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -21,4 +21,15 @@ module.exports = {
     }),
   ],
   mode: 'development',
+  //  webpack 命令会直接打包，   在局部的命令中，  node_modules/.bin/webpack  来打包
+  //开发服务器， 用来自动化打包， 自动刷新， 自动打开浏览器( 自动监视， 只会在内存打包， 不会输出)
+  // 开启服务器， 命令 npx webpack-deb-server
+  devServer: {
+    static: {
+      directory: resolve(__dirname, 'webpack-npm-git/build'),
+    },
+    compress: true,
+    port: 3000,
+    open: true,
+  },
 }
